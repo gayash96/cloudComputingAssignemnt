0fed7dd530d6f7c1c158e399da144b770e4613ef
gayash96 <35984444+gayash96@users.noreply.github.com>
2023-05-09 06:27:02 +0530

    Updated the html pages

--- application.py
+++ application.py
@@ -2,19 +2,33 @@ import os
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
+import json
+import urllib.request
+import certifi
 
 application = Flask(__name__)
 
 
+@application.route('/liveScores')
+def liveScores():
+   response = urllib.request.urlopen("https://api.cricapi.com/v1/currentMatches?apikey=9eb76118-7292-43f8-93eb-6f0ab8f19bf2&offset=0", cafile=certifi.where())
+   matches = []
+   matchData = json.loads(response.read().decode())['data']
+   for match in matchData:
+      matches.append({
+         'name': match['name'],
+         'status': match['status'],
+         'venue': match['venue'],
+         'date': match['date'],
+         'score': match['score']
+      })
+   print(matches)
+   return render_template('liveScores.html', matches=matches)
+
 @application.route('/')
 def index():
    print('Request for index page received')
-   return render_template('liveScores.html')
-
-@application.route('/liveScores')
-def liveScores():
-   print('Request for index page received')
-   return render_template('liveScores.html')
+   return render_template('index.html')
 
 
 @application.route('/hello', methods=['POST'])
