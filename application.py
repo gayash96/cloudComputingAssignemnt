5918329c7f125c02a004a9195e5e369695005eeb
gayash96 <35984444+gayash96@users.noreply.github.com>
2023-05-14 05:12:33 +0530

    added upcoming matches section

--- application.py
+++ application.py
@@ -42,6 +42,23 @@ def scoreBoard():
    print(matches)
    return render_template('scoreBoard.html', matches=matches)
 
+@application.route('/upcomingMatches')
+def upcomingMatches():
+   response = urllib.request.urlopen("https://api.cricapi.com/v1/cricScore?apikey=9eb76118-7292-43f8-93eb-6f0ab8f19bf2", cafile=certifi.where())
+   matches = []
+   matchData = json.loads(response.read().decode())['data']
+   for match in matchData:
+      matches.append({
+         'name': match['t1'] + ' vs '+ match['t2'],
+         'status': match['status'],
+         'score': match['t1'] + ' : ' + match['t1s']+ ' ' +match['t2'] + ' : ' + match['t2s'],
+         'date': match['dateTimeGMT'],
+         'matchType': match['matchType'],
+         'ms': match['ms']
+      })
+   print(matches)
+   return render_template('upcomingMatches.html', matches=matches)
+
 @application.route('/')
 def index():
    print('Request for index page received')
